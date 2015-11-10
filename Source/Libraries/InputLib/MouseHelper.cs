0d4f27f487187bf29d7552d827152e6544626e88
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-11-10 07:00:26 -0600

    Changed name of input library.

--- Source/Libraries/InputLib/MouseHelper.cs
+++ Source/Libraries/InputLib/MouseHelper.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace XnaInput
 {
--- Source/Libraries/InputLib/XnaInput.cs
+++ Source/Libraries/InputLib/XnaInput.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace XnaInput
 {
