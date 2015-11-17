580863378325790787a41291587837a01d6f0733
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-11-17 15:54:58 -0600

    Messing around with input library.

---
+++ Source/Libraries/YpsilonInput/YpsilonInput/Core/Input/InputEventMouse.cs
@@ -0,0 +1,106 @@
+﻿/***************************************************************************
+ *   InputEventMouse.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
+ *   
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+using Microsoft.Xna.Framework;
+using Ypsilon.Core.Windows;
+
+namespace Ypsilon.Core.Input
+{
+    public class InputEventMouse : InputEvent
+    {
+        public bool IsEvent(MouseEvent e, MouseButton b)
+        {
+            if (e == this.EventType && b == this.Button)
+                return true;
+            return false;
+        }
+
+        private readonly MouseEvent m_eventType;
+        public MouseEvent EventType
+        {
+            get { return m_eventType; }
+        }
+
+        private const int WHEEL_DELTA = 120;
+        public int WheelValue
+        {
+            get { return (m_clicks / WHEEL_DELTA); }
+        }
+
+        private readonly WinMouseButtons m_button;
+        private readonly int m_clicks;
+        private readonly int m_mouseData;
+        private readonly int m_x;
+        private readonly int m_y;
+
+        public MouseButton Button
+        {
+            get
+            {
+                if ((m_button & WinMouseButtons.Left) == WinMouseButtons.Left)
+                    return MouseButton.Left;
+                if ((m_button & WinMouseButtons.Right) == WinMouseButtons.Right)
+                    return MouseButton.Right;
+                if ((m_button & WinMouseButtons.Middle) == WinMouseButtons.Middle)
+                    return MouseButton.Middle;
+                if ((m_button & WinMouseButtons.XButton1) == WinMouseButtons.XButton1)
+                    return MouseButton.XButton1;
+                if ((m_button & WinMouseButtons.XButton2) == WinMouseButtons.XButton2)
+                    return MouseButton.XButton2;
+                return MouseButton.None;
+            }
+        }
+
+        public int MouseData
+        {
+            get { return m_mouseData; }
+        }
+
+        public int X
+        {
+            get { return m_x; }
+        }
+
+        public int Y
+        {
+            get { return m_y; }
+        }
+
+        public Point Position
+        {
+            get { return new Point(m_x, m_y); }
+        }
+
+        public InputEventMouse(MouseEvent eventType, WinMouseButtons button, int clicks, int x, int y, int mouseData, WinKeys modifiers)
+            : base(modifiers)
+        {
+            Vector2 dpi = DpiManager.GetSystemDpiScalar();
+
+            m_eventType = eventType;
+            m_button = button;
+            m_clicks = clicks;
+            m_x = (int)(x / dpi.X);
+            m_y = (int)(y / dpi.Y);
+            m_mouseData = mouseData;
+        }
+
+        public InputEventMouse(MouseEvent eventType, InputEventMouse parent)
+            : base(parent)
+        {
+            m_eventType = eventType;
+            m_button = parent.m_button;
+            m_clicks = parent.m_clicks;
+            m_x = parent.m_x;
+            m_y = parent.m_y;
+            m_mouseData = parent.m_mouseData;
+        }
+    }
+}
---
+++ Source/Libraries/YpsilonInput/YpsilonInput/Core/Windows/NativeMethods.cs
@@ -0,0 +1,114 @@
+﻿/***************************************************************************
+ *   NativeMethods.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+#region usings
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+#endregion
+
+namespace Ypsilon.Core.Windows
+{
+    public delegate IntPtr WndProcHandler(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+    class NativeMethods
+    {
+        [DllImport("Kernel32")]
+        private unsafe static extern int _lread(SafeFileHandle hFile, void* lpBuffer, int wBytes);
+
+        internal static unsafe void ReadBuffer(SafeFileHandle ptr, void* buffer, int length)
+        {
+            _lread(ptr, buffer, length);
+        }
+
+        [DllImport("Imm32.dll")]
+        internal static extern IntPtr ImmGetContext(IntPtr hWnd);
+
+        [DllImport("Imm32.dll")]
+        internal static extern IntPtr ImmAssociateContext(IntPtr hWnd, IntPtr hIMC);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+        internal static extern int MultiByteToWideChar(int CodePage, int dwFlags, byte[] lpMultiByteStr, int cchMultiByte, char[] lpWideCharStr, int cchWideChar);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        internal static extern short GetKeyState(int keyCode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        internal static extern IntPtr SetWindowsHookEx(int hookType, WndProcHandler callback, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        internal static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        internal static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetMessage(out Message lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        [DllImport("user32.dll", EntryPoint = "TranslateMessage")]
+        internal static extern bool TranslateMessage(ref Message m);
+
+        [DllImport("user32.dll")]
+        internal static extern uint GetWindowThreadProcessId(IntPtr window, IntPtr module);
+
+        internal static int LOWORD(IntPtr val)
+        {
+            return (unchecked((int)(long)val)) & 0xFFFF;
+        }
+
+        internal static int MAKELCID(int languageID, int sortID)
+        {
+            return ((0xFFFF & languageID) | (((0x000F) & sortID) << 16));
+        }
+
+        internal static int MAKELANGID(int primaryLang, int subLang)
+        {
+            return ((((ushort)(subLang)) << 10) | (ushort)(primaryLang));
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern int GetLocaleInfo(int locale, int lcType, out uint lpLCData, int cchData);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetKeyboardLayout(uint idThread);
+
+        internal static uint GetCurrentCodePage()
+        {
+            // Get the keyboard layout for the current thread.
+            IntPtr keybdLayout = GetKeyboardLayout(0);
+
+            // Extract the language ID from it, contained in its low-order word.
+            int langID = LOWORD(keybdLayout);
+
+            // Call the GetLocaleInfo function to retrieve the default ANSI code page
+            // associated with that language ID.
+            uint codePage = 0;
+            GetLocaleInfo(MAKELCID(langID, NativeConstants.SORT_DEFAULT),
+                           NativeConstants.LOCALE_IDEFAULTANSICODEPAGE | NativeConstants.LOCALE_RETURN_NUMBER,
+                           out codePage,
+                           Marshal.SizeOf(codePage));
+            return codePage;
+        }
+    }
+}
