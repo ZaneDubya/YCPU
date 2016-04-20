7809c0f082ca2de20c2fed2e3a6aed787dd237da
Zane Wagner <ZaneDubya@users.noreply.github.com>
2016-04-20 09:25:02 -0500

    Adding new string extension 'truncate'.

---
+++ Source/Libraries/YpsilonFramework/Extensions/StringX.cs
@@ -0,0 +1,11 @@
+﻿namespace Ypsilon.Extensions
+{
+    public static class StringX
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}
